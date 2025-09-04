cee1f6c75f286d653065ef792485ce513a728744
acefrogge <123892168+acefrogge@users.noreply.github.com>
2025-09-04 22:34:37 +0200

    remove all nbsp's

--- scripts/update-updates-feed.py
+++ scripts/update-updates-feed.py
@@ -3,7 +3,6 @@ import sys
 import locale
 import json
 import feedparser
-import hashlib
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -15,7 +14,7 @@ from feedgen.feed import FeedGenerator
 from datetime import datetime
 
 base_url = 'https://www.counter-strike.net'
-content_url = base_url + '/news/updates'
+content_url = base_url + '/news'
 
 # Define a list of languages to fetch and parse
 language_map = {
@@ -41,57 +40,82 @@ for language_name, (language_code, language_locale) in language_map.items():
 
         # Wait for the contents to appear (thanks Valve for using reactJS)
         element = WebDriverWait(driver, 15).until(
-            EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div/div/div[3]/div[2]/div[1]/div[1]'))
+            EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div/div/div[3]/div[2]/div[1]'))
         )
 
         # Extract the (hopefully) complete HTML
         html_content = driver.page_source
     except TimeoutException:
         driver.quit()
-        sys.exit(f'Unable to find the updates container in the given time frame.')
+        sys.exit(f'Unable to find the news capsule container in the given time frame.')
     except Exception as e:
         driver.quit()
+        print(e)
         sys.exit(f'Failed to extract the HTML data.')
 
     # Parse the HTML content with BeautifulSoup and extract all relevant information
     soup = BeautifulSoup(html_content, 'html.parser')
 
-    # Find all div containers that correspond to update capsules using XPath
+    # Find all containers with class names that contain "blogcapsule_BlogCapsule"
     capsule_divs = []
     for i in range(1, 16):
         try:
-            capsule = driver.find_element(By.XPATH, f'/html/body/div[2]/div/div/div[3]/div[2]/div[1]/div[{i}]')
+            capsule = driver.find_element(By.XPATH, f'/html/body/div[2]/div/div/div[3]/div[2]/div[1]/a[{i}]')
             capsule_divs.append(capsule)
         except:
             break 
 
-    # Create an array of all update items
-    updates = []
+    # Create an array of all news entries
+    news_items = []
 
     # Set locale to parse the date, but dates are currently not localized anyways (Thanks Valve)
     locale.setlocale(locale.LC_TIME, f'en_US.UTF-8') # Switch to language_locale after it's fixed (if ever)
     date_format = '%B %d, %Y' # English
-    
     #locale.setlocale(locale.LC_TIME, 'de_DE') # German
     #date_format = '%d. %B %Y' # German
 
-    # For each update capsule, find all div containers with relevant information
+    # For each news capsule, open the entry and find all div containers with relevant information
     for capsule in capsule_divs:
-        date_text = capsule.find_element(By.XPATH, './div[1]').text.strip()
-        date = datetime.strptime(date_text, date_format)
-        title = capsule.find_element(By.XPATH, './div[2]').text.strip()
-        desc = capsule.find_element(By.XPATH, './div[3]').get_attribute('innerHTML').strip()
+        unique_relative_url = capsule.get_attribute('href')
+        unique_url = f'{base_url}{unique_relative_url}?l={language_name}'
+        unique_identifier = unique_relative_url.replace('/newsentry/', '')
 
-        # Remove trailing <br/> tags at the beginning of the update description (Thanks Valve)
+        # Navigate the browser to the news entry
+        try:
+            driver.get(unique_url)
+
+            # Wait for the contents to appear
+            element = WebDriverWait(driver, 15).until(
+                EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div/div'))
+            )
+
+            # Extract the (hopefully) complete HTML
+            news_html_content = driver.page_source
+        except TimeoutException:
+            driver.quit()
+            sys.exit(f'Unable to open or find the news entry page in the given time frame.')
+        except Exception as e:
+            driver.quit()
+            sys.exit(f'Failed to extract the HTML data of a news entry.')
+
+        title = capsule.find_element(By.XPATH, './div[3]').text.strip()
+        date_text = capsule.find_element(By.XPATH, './div[4]').text.strip()
+        date = datetime.strptime(date_text, date_format)
+        desc = capsule.find_element(By.XPATH, './div[5]').get_attribute('innerHTML').strip()
+
+        # Remove trailing <br/> tags at the beginning of the news article
         while desc.startswith('<br'):
             index = desc.index('>') + 1
             desc = desc[index:]
 
         # Replace all <wbr> with <br>
         desc = desc.replace('<wbr>', '<br>')
+        # Replace all nbsp's
+        desc = desc.replace('&nbsp;', ' ')
 
-        updates.append({
-            'guid': hashlib.sha256(f'{date.day}{date.month}{date.year}'.encode()).hexdigest(),
+        news_items.append({
+            'guid': unique_identifier,
+            'url': unique_url,
             'title': title,
             'date': date,
             'content': desc
@@ -101,46 +125,44 @@ for language_name, (language_code, language_locale) in language_map.items():
     github_workspace = os.getenv('GITHUB_WORKSPACE')
 
     if github_workspace:
-        rss_feed_file = os.path.join(os.environ['GITHUB_WORKSPACE'], 'feeds', f'updates-feed-{language_code}.xml')
+        rss_feed_file = os.path.join(os.environ['GITHUB_WORKSPACE'], 'feeds', f'news-feed-{language_code}.xml')
     else:
-        rss_feed_file = os.path.join(os.pardir, 'feeds', f'updates-feed-{language_code}.xml')
+        rss_feed_file = os.path.join(os.pardir, 'feeds', f'news-feed-{language_code}.xml')
 
     skip_file = False
 
     if os.path.exists(rss_feed_file):
         current_feed = feedparser.parse(rss_feed_file)
-        if current_feed.entries and updates:
-            current_description = current_feed.entries[0].description[:250]
-            new_description = updates[0]['content'][:250]
-            if current_description == new_description:
-                skip_file = True
+        if current_feed.entries and news_items and current_feed.entries[0].title == news_items[0]['title']:
+            skip_file = True
 
     # Generate the RSS feed with feedgen if the latest entry is different from the current RSS feed
     if not skip_file:
-        feed_link = f'https://raw.githubusercontent.com/acefrogge/CS-RSS-Feed/master/feeds/updates-feed-{language_code}.xml'
+        feed_link = f'https://raw.githubusercontent.com/acefrogge/CS-RSS-Feed/master/feeds/news-feed-{language_code}.xml'
 
         fg = FeedGenerator()
-        fg.title(f'Counter-Strike 2 - Updates ({language_name.capitalize()})')
-        fg.description('Counter-Strike 2 Updates Feed')
+        fg.title(f'Counter-Strike 2 - News ({language_name.capitalize()})')
+        fg.description('Counter-Strike 2 News Feed')
         fg.link(href=feed_link, rel='self')
         fg.language(language_code)
 
         # Add the extracted information as entries to the RSS feed
-        for update in reversed(updates):
+        for news in reversed(news_items):
             fe = fg.add_entry()
             fe.source(url)
-            fe.guid(update['guid'])
-            fe.title(update['title'])
+            fe.id(news['url'])
+            fe.guid(news['guid'])
+            fe.title(news['title'])
             fe.link({
-                'href': url,
+                'href': news['url'],
                 'rel': 'alternate',
                 'type': 'text/html',
                 'hreflang': language_code,
-                'title': update['title']
+                'title': news['title']
             })
-            fe.pubDate(datetime.strftime(update['date'], '%Y-%m-%dT%H:%M:%SZ'))
+            fe.pubDate(datetime.strftime(news['date'], '%Y-%m-%dT%H:%M:%SZ'))
             fe.author({'name':'Valve Corporation', 'email':'support@steampowered.com'})
-            fe.content(update['content'], None, 'CDATA')
+            fe.content(news['content'], None, 'CDATA')
             fe.rights('Valve Corporation')
 
         rss_content = fg.rss_str(pretty=True)
